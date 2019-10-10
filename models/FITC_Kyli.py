96efd9ab5263c8871222795406b422e5cb04d65d
Kyli McKay-Bishop <kyli.nmb@gmail.com>
2019-10-10 16:47:14 -0600

    tensorflow throwing exception, matrix not invertible

--- models/FITC_Kyli.py
+++ models/FITC_Kyli.py
@@ -18,7 +18,7 @@ class FITC:
         self.tf_y = tf.compat.v1.placeholder(tf.float32, shape=[None, 1])
         self.tf_Xt = tf.compat.v1.placeholder(tf.float32, shape=[None, self.d])
         # model parameters
-        self.tf_Xbar = tf.Variable(tf.zeros([10, self.d]), dtype=tf.float32)  #TODO what is the right number for M
+        self.tf_Xbar = tf.Variable(tf.ones([50, self.d]), dtype=tf.float32)  #TODO what is the right number for M
         self.tf_log_length_scale = tf.Variable(0.0, dtype=tf.float32)
         self.tf_log_amp = tf.constant(0.0, dtype=tf.float32)
         self.tf_log_tau = tf.Variable(0.0, dtype=tf.float32)
@@ -41,21 +41,28 @@ class FITC:
         k = tf.exp(self.tf_log_amp) * tf.exp(-1.0/tf.exp(self.tf_log_length_scale) * k)
         return k
 
-    def kernel_matrix_NM(self):
+    def kernel_matrix_MN(self):
         col_norm_bar = tf.reduce_sum(self.tf_Xbar * self.tf_Xbar, 1)
         col_norm_bar = tf.reshape(col_norm_bar, [-1, 1])
         col_norm_n = tf.reduce_sum(self.tf_X * self.tf_X, 1)
-        col_norm_n = tf.reshape(col_norm_n, [-1, 1])
-        k = col_norm_bar + col_norm_n - 2.0 * tf.matmul(self.tf_X, tf.transpose(self.tf_Xbar))
+        col_norm_n = tf.transpose(tf.reshape(col_norm_n, [-1, 1]))
+        k = col_norm_bar + col_norm_n - 2.0 * tf.matmul(self.tf_Xbar, tf.transpose(self.tf_X))
         k = tf.exp(self.tf_log_amp) * tf.exp(-1.0/tf.exp(self.tf_log_length_scale) * k)
         return k
 
+    def bigLambda(self, km, kmn):
+        l = tf.transpose(kmn) * tf.transpose(tf.matmul(tf.matrix_inverse(km), kmn))
+        l = tf.reduce_sum(l, 1)
+        l = 1 - l
+        l = tf.matrix_diag(l)
+        return l
+
     def neg_log_likelihood(self):
-        Knm = self.kernel_matrix_NM()
+        Kmn = self.kernel_matrix_MN()
         Km = self.kernel_matrix_M()
-        Lambda = 1 - tf.reduce_sum(tf.math.multiply(tf.transpose(Knm), tf.transpose(tf.linalg.solve(Km, Knm))), 1)
-        Lambda = tf.linalg.diag(Lambda)
-        S = Knm*tf.linalg.solve(Km, tf.transpose(Knm)) + Lambda + 1.0/tf.exp(self.tf_log_tau)*tf.eye(10) # TODO not sure if the should be N or M
+        kernels = tf.matmul(tf.transpose(Kmn), tf.linalg.solve(Km, Kmn))
+        sigma2 = 1.0/tf.exp(self.tf_log_tau)*tf.eye(self.N)
+        S = kernels + self.bigLambda(Km, Kmn) + sigma2
         L = 0.5*tf.linalg.logdet(S) + 0.5*tf.matmul(tf.transpose(self.tf_y), tf.linalg.solve(S, self.tf_y))[0, 0]
         return L
 
